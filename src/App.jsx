cc3c220a2c694be048fdfac04faba4e48016027f
Maria Isabelly Andrade <andradeisabellypessoal@gmail.com>
2026-04-08 12:11:11 -0300

    Atualizando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import CadastroBrutus from "./Barbeiro/CadastroBrutus";
 import ListaBarbeiros from "./Barbeiro/ListaBarbeiros";
 import CadastroServico from "./Servicos/CadastrarServico";
 import ListaServicos from "./Servicos/ListaServico";
+import React from "react";
+import ReactDOM from 'react-dom/client'
 
-function App() {
-  return <CadastroServico/>;
-}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <ListaServicos/>
+  </React.StrictMode>
+)
+
+/*function App() {
+  return <ListaServicos/>;
+}*/
 
 export default App;
